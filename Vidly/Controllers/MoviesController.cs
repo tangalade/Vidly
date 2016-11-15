ae4798712f26109e92b885ac081ce94e93dc31d6
kbilbray <kylebilbray@gmail.com>
2016-11-14 19:05:45 -0600

    Up through Section 3, Lecture 32

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public List<Movie> movies = new List<Movie>
+        {
+            new Movie { Name = "Shrek" },
+            new Movie { Name = "Wall-e" }
+        };
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int movieid)
+        {
+            return Content("id=" + movieid);
+        }
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View(movies);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
